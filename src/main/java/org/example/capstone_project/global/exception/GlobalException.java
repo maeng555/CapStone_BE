cdf41a20335ab045388492d15ebfafd9c9e549bf
maeng555 <mjy5188@naver.com>
2025-04-17 23:25:15 +0900

    "Feat: global 예외처리 구현 "

---
+++ src/main/java/org/example/capstone_project/global/exception/GlobalException.java
@@ -0,0 +1,35 @@
+package org.example.capstone_project.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    // @Valid 실패 시
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    // IllegalArgumentException (로그인 등)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArg(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body("❌ " + ex.getMessage());
+    }
+
+
+
+
+}
