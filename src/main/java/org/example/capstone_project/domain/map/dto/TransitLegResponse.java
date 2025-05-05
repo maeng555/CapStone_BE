e3376f86fafd3aa2a4de563509cab50cdf188e9c
maeng555 <mjy5188@naver.com>
2025-05-05 15:29:48 +0900

    chore: resoponse 수정

--- src/main/java/org/example/capstone_project/domain/map/dto/TransitLegResponse.java
+++ src/main/java/org/example/capstone_project/domain/map/dto/TransitLegResponse.java
@@ -12,12 +12,12 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class TransitLegResponse {
-    private String mode;
+    private String mode; // walk, bus, subway
     private String route; //노선
-    private int sectionTime;
-    private int distance;
-    private String startName;
-    private String endName;
+    private int sectionTime;  // 노선 시간
+    private int distance; //거리
+    private String startName; //입력지
+    private String endName; //도착지
     private List<String> stations; // SUBWAY, BUS용
     private Integer stationCount; // SUBWAY, BUS용
     private List<String> descriptions; // WALK용
