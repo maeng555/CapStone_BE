73920f9568a18618bbdc36268b9adfd55517bfca
maeng555 <mjy5188@naver.com>
2025-06-05 01:39:48 +0900

    "feat: 사용자 검색키워드 저장 엔티티 생성   "

---
+++ src/main/java/org/example/capstone_project/domain/food/entity/UserSearchKeyword.java
@@ -0,0 +1,29 @@
+package org.example.capstone_project.domain.food.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.capstone_project.domain.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_search_keyword")
+public class UserSearchKeyword {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String keyword;
+
+    @Column(nullable = false)
+    private LocalDateTime searchedAt = LocalDateTime.now();
+}
